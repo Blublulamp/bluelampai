7bab75870fa99859fcb601a9688973f1f6e40b7d
Blublulamp <lukmanaerana6629@gmail.com>
2026-09-11 13:54:16 +0630

    Update script.js

--- script.js
+++ script.js
@@ -3913,7 +3913,7 @@ let historyPageState = null;
 async function loadCloudMessages(chatId, before = null) {
   const query = new URLSearchParams({
     chat_id: String(chatId),
-    limit: "2"
+    limit: "50"
   });
 
   if (before !== null) query.set("before", String(before));
